7602417756208039e2e2ad57db8ad65ab9686dbd
Drozdov96 <drozdov.na96@gmail.com>
2019-03-23 16:28:30 +0400

    add validation of token and requestMessage

--- controllers/Application.php
+++ controllers/Application.php
@@ -1,13 +1,12 @@
 <?php
-
-namespace Models;
+namespace Controllers;
 
 
 class Application
 {
     private $database;
     private $requestArr;
-    const BOT_TOKEN = '899053884:AAGbwi3l7_FodfZC2knlxUkYiqsAhJTXNio';
+    private $botToken;
 
     public function __construct()
     {
@@ -24,8 +23,25 @@ class Application
         //$this->database
     }
 
-    public function run()
+    public function run(string $jsonRequest, string $botToken)
     {
+        if($this->validateBotToken($botToken)){
+            $this->botToken = $botToken;
+        }else{
+            return;
+        }
+
+        $this->requestArr = $this->processRequest($jsonRequest);
+        if(empty($this->requestArr['result'])){
+            return;
+        }else{
+            $requestMessageArray = $this->parseRequestMessage($this->requestArr[]);
+        }
+
+        if(!$this->validateRequest($requestMessageArray)){
+            return;
+        }
+        
         foreach ($this->requestArr['result'] as $value){
             $messageRequest = curl_init("https://api.telegram.org/bot".$this::BOT_TOKEN."/sendMessage?chat_id={$value['message']['chat']['id']}&text={$value['message']['text']}");
             curl_setopt($messageRequest, CURLOPT_RETURNTRANSFER, 1);
@@ -42,5 +58,44 @@ class Application
     public function requestIsEmpty()
     {
         return empty($this->requestArr['result']);
+    }
+
+    private function validateBotToken (string $token): bool
+    {
+        $tokensFile = fopen($_SERVER['DOCUMENT_ROOT'].'/files/bots.txt', 'r');
+
+        while (($referenceToken = fgets($tokensFile)) !== false){
+            if($referenceToken === $token){
+                fclose($tokensFile);
+                return true;
+            }
+        }
+        fclose($tokensFile);
+        return false;
+    }
+    
+    private function processRequest(string $jsonRequest): array
+    {
+        return  $requestArray = json_decode($jsonRequest);
+    }
+
+    private function parseRequestMessage(array $request): array
+    {
+        $requestMessage = trim($request['result']['message']['text']);
+        return $requestArray = preg_split('\s+', $requestMessage);
+    }
+
+    private function validateRequest(array $requestArr): bool
+    {
+        if(preg_match("/[A-Za-z]+",array_shift($requestArr)) !== 1){
+            return false;
+        }
+
+        foreach ($requestArr as $value){
+            if(preg_match("[A-Za-z.()]+", $value) !== 1){
+                return false;
+            }
+        }
+        return true;
     }
 }
